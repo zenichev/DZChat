8ac44d93f3f730c9a0a221599a75f4b11acf16b3
zenichev <34710178+zenichev@users.noreply.github.com>
2018-09-21 11:20:14 +0300

    Update main.h

--- server/main.h
+++ server/main.h
@@ -5,10 +5,10 @@
 
 /* ----- PRE-PROCESSOR DIRECTIVE ----- */
 #define SERVER_PORT 5000		// default port for listening
-#define SERVER_IP "127.0.0.1"	// ip to listen for
+#define SERVER_IP "127.0.0.1"		// ip to listen for
 #define MAX_AVAILABLE 10		// maximum available clients
 #define RCVBUFSIZE 50000		// receive buffer for each message
-#define ID_STRING "z9HG4bk:"	// string used to identify identification process
+#define ID_STRING "z9HG4bk:"		// string used to identify identification process
 #define NAME_SIZE 20			// acceptable name size
 #define CURTIME time(&currentTime.rawtime);currentTime.info=localtime(&currentTime.rawtime);strftime(currentTime.buffer,80,"%I:%M:%S%p", currentTime.info)
 /* ----------------------------------- */
@@ -19,10 +19,10 @@
 typedef struct clients
 {
 	char nickname[NAME_SIZE];	// client name
-	int client_id;				// client id
-	int socket;					// client socket openned
-	char ip[15];				// ip address of a connected client
-	int port;					// port where client listens for direct connections from others
+	int client_id;			// client id
+	int socket;			// client socket openned
+	char ip[15];			// ip address of a connected client
+	int port;			// port where client listens for direct connections from others
 	unsigned int counter;		// id for each client thread
 } client_parameters;
 
@@ -49,21 +49,21 @@ typedef struct getCurrentTime		// for timing in the messages
 int addPort(void);																	// ask user of the server for port
 int socketDescriptor(void);															// open socker descriptor
 void bindServer(int descriptor,struct sockaddr_in *structName,int size);			// bind socket to a port
-void listenServer(int descriptor);													// start listening
-void sinStructure(struct sockaddr_in *structName,int port);							// ip address/port structure for clients/server
+void listenServer(int descriptor);								// start listening
+void sinStructure(struct sockaddr_in *structName,int port);					// ip address/port structure for clients/server
 
 /* Client processing related */
-int acceptConnection(int descriptor,struct sockaddr_in *structName,int *length);	// wait for connection from a client
-void * handleTCPClient(void * arguments);											// handle client in a new thread
+int acceptConnection(int descriptor,struct sockaddr_in *structName,int *length);		// wait for connection from a client
+void * handleTCPClient(void * arguments);							// handle client in a new thread
 char userIdentification(char * buffer,int size,char * name,char * port);			// get a client's name and a listening port (clients have separate port for listening)
 void notificationOnline(int * list, struct clients * shm, int * socket, int online,  unsigned int * counter);// notify a new client which online do we have now
 //
-void chatting(char * buffer, int * socket, client_parameters * list, int * idList, unsigned int * counter);					// get informational requests from clients and answer on them
+void chatting(char * buffer, int * socket, client_parameters * list, int * idList, unsigned int * counter);			// get informational requests from clients and answer on them
 void giveUpdates(char * sourceString, int * socket, client_parameters * parameters, int * idList, unsigned int * counter); 	// send user updates
 //
-int onlineCounter(int action);														// clients counter
-int randomID(void);																	// generate ID for clients
+int onlineCounter(int action);													// clients counter
+int randomID(void);														// generate ID for clients
 //
-int * setCurrentList(int *passedID, int action, struct clients * infoClients);		// keep a list of client IDs
+int * setCurrentList(int *passedID, int action, struct clients * infoClients);							// keep a list of client IDs
 client_parameters * clientsStructure(char * name, int * myID, int * socket, char * portListening, char * ip, int usersOnline, unsigned int * counter);	// keep clients data: name, ids, socket etc. in an array of structures
 /* ----------------------------------- */
