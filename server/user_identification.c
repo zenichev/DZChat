9b00369e402d57ed6fdb77be1ec7b9c33556d227
zenichev <34710178+zenichev@users.noreply.github.com>
2018-09-21 12:00:19 +0300

    Update user_identification.c

--- server/user_identification.c
+++ server/user_identification.c
@@ -8,9 +8,9 @@
 /* Retrieve from a user name and his listening port (not the port used to connect to us) */
 /*----------------------------------------------------------------------------------------------------------------------------*/
 char userIdentification(char * buffer,int size,char * name,char * port) {
-	char *p=0; p=buffer;	// set a pointer to a buffer, to catch a name of the user in a char array
+	char *p=0; p=buffer;		// set a pointer to a buffer, to catch a name of the user in a char array
 	char *n=0; n=name;		// set a pointer to a name array
-	char *po=0; po=port;	// set a pointer to a port array
+	char *po=0; po=port;		// set a pointer to a port array
 	int length=NAME_SIZE, namesize=0, portLengthAcceptable=6, portLengthReal=0;	// set limiter for while loop ; used to count a name size ; size of the port;
 	
 	int moveto = sizeof(ID_STRING);	// moving integer
@@ -28,19 +28,19 @@ char userIdentification(char * buffer,int size,char * name,char * port) {
 	}
 
 	/* get client port */
-	if ( strchr(":",*p) ) {   // colon is a delimiter for name and port
-		p++;                    // move pointer from colon to a beginning port symbol
+	if ( strchr(":",*p) ) {				// colon is a delimiter for name and port
+		p++;                    		// move pointer from colon to a beginning port symbol
 		while (*p && portLengthAcceptable-->0)
 		{
 			if(strchr("0123456789",*p)) {
-				*po++=*p++;         // copy port symbols to a port variable
+				*po++=*p++;		// copy port symbols to a port variable
 				portLengthReal++;
 			} else {
-				p++;                // if the char is not acceptable just move to another one
+				p++;			// if the char is not acceptable just move to another one
 			}
 		}
 	}
-	p -= (namesize+moveto+portLengthReal);  // move a pointer back to a beginning
-	memset(p,0,size);                       // set passed buffer to null
+	p -= (namesize+moveto+portLengthReal);		// move a pointer back to a beginning
+	memset(p,0,size);				// set passed buffer to null
 }
 /*----------------------------------------------------------------------------------------------------------------------------*/
