232102b355caa8031709d4107f873df27a72e0c5
zenichev <34710178+zenichev@users.noreply.github.com>
2018-09-25 11:17:00 +0300

    Update client_handler.c

--- server/client_handler.c
+++ server/client_handler.c
@@ -1,11 +1,11 @@
 /* DZChat - Server. Sep 2018 @Donat Zenichev */
 /* thread handler - each client is processed in a separate thread */
 
-#include <stdio.h>      /* for printf() and fprintf() */
-#include <string.h>     /* different manipulations over a char arrays, such as memset() */
-#include <sys/socket.h> /* for socket(), bind(), connect(), recv() and send() */
-#include <sys/types.h>  /* data types */
-#include "main.h"       /* include my custom headers and definitions */
+#include <stdio.h>		/* for printf() and fprintf() */
+#include <string.h>		/* different manipulations over a char arrays, such as memset() */
+#include <sys/socket.h>		/* for socket(), bind(), connect(), recv() and send() */
+#include <sys/types.h>		/* data types */
+#include "main.h"		/* include my custom headers and definitions */
 
 /* ----------------------------------- HANDLER FOR CLIENTS ----------------------------------- */
 void * handleTCPClient(void * arguments)
@@ -15,13 +15,13 @@ void * handleTCPClient(void * arguments)
 
 	/* passed structure to a thread */
 	handle_client * initial = (handle_client *) arguments;
-	int socket = (int ) initial->socket;                        // socket used to process clients
-	char * ip = (char *) initial->ip;                           // ip address of a connected client
-	unsigned int counter = (unsigned int) initial->counter;     // ip address of a connected client
+	int socket = (int ) initial->socket;					// socket used to process clients
+	char * ip = (char *) initial->ip;					// ip address of a connected client
+	unsigned int counter = (unsigned int) initial->counter;			// ip address of a connected client
 
-	client_parameters * clientStructuresList;                   // for client parameters: name, id, socket, ip, port
-	int membersOnline=0, myID=0, recvMsgSize=0;                 // members online counter; id for a current client; size of a received message;
-	char echoBuffer[RCVBUFSIZE], name[NAME_SIZE], port[6];      // Buffer for echo string; acceptable name size; clients binded port;
+	client_parameters * clientStructuresList;				// for client parameters: name, id, socket, ip, port
+	int membersOnline=0, myID=0, recvMsgSize=0;				// members online counter; id for a current client; size of a received message;
+	char echoBuffer[RCVBUFSIZE], name[NAME_SIZE], port[6];			// Buffer for echo string; acceptable name size; clients binded port;
 
 	memset(name,0,sizeof(name));
 	memset(echoBuffer,0,sizeof(echoBuffer));
@@ -32,22 +32,22 @@ void * handleTCPClient(void * arguments)
 
 	// User identification: get user name, user listening port
 	if (strstr(echoBuffer,ID_STRING)) {
-		userIdentification(echoBuffer,sizeof(echoBuffer),name,port);// echoBuffer is cleared in the identification function
+		userIdentification(echoBuffer,sizeof(echoBuffer),name,port);	// echoBuffer is cleared in the identification function
 		CURTIME;
 		printf("%s INFO <%d>: The client identified himself as %s\n", currentTime.buffer, counter, name);
 	}
 	memset(echoBuffer,0,sizeof(echoBuffer));
 	//
-	myID=randomID();                  // generate new id for a current client
-	membersOnline=onlineCounter(1);   // keep an online counter actual
+	myID=randomID();							// generate new id for a current client
+	membersOnline=onlineCounter(1);						// keep an online counter actual
 	//
-	clientStructuresList=clientsStructure(name, &myID, &socket, port, ip, membersOnline, &counter); // update array of structures
-	int * currentList=setCurrentList(&myID, 1, clientStructuresList);                               // add client to a list of IDs
-	notificationOnline(currentList, clientStructuresList, &socket, membersOnline, &counter);        // Notify user, about who is online now
-	chatting(echoBuffer, &socket, clientStructuresList, currentList , &counter);                    // Receive/send messages
+	clientStructuresList=clientsStructure(name, &myID, &socket, port, ip, membersOnline, &counter);	// update array of structures
+	int * currentList=setCurrentList(&myID, 1, clientStructuresList);								// add client to a list of IDs
+	notificationOnline(currentList, clientStructuresList, &socket, membersOnline, &counter);	// Notify user, about who is online now
+	chatting(echoBuffer, &socket, clientStructuresList, currentList , &counter);						// Receive/send messages
 	//
-	onlineCounter(0);                                 // keep an online counter actual
-	setCurrentList(&myID, 0, clientStructuresList);   // delete user ID from the list of IDs and delete an information from the structure
+	onlineCounter(0);										// keep an online counter actual
+	setCurrentList(&myID, 0, clientStructuresList);							// delete user ID from the list of IDs and delete an information from the structure
 	// clear variables
 	memset(name,0,sizeof(name));
 	memset(echoBuffer,0,sizeof(echoBuffer));
