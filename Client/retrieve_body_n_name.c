1a34e85e90fd49646b2b8b99b9b090c4a7c71f37
zenichev <34710178+zenichev@users.noreply.github.com>
2018-09-25 10:46:05 +0300

    Create retrieve_body_n_name.c

---
+++ Client/retrieve_body_n_name.c
@@ -0,0 +1,49 @@
+/* DZChat - Client. Sep 2018 @Donat Zenichev */
+/* Process a user's entered data and get a destination name from it and a message body */
+
+/* HEADER FILE INCLUDES */
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <string.h>     /* different manipulations over a char arrays, such as memset() */
+#include "main.h"       /* Include defenitions */
+
+/* ------------------ RETRIEVE DATA FROM AN USER MESSAGE ----------------------------------- */
+// get a recipient name
+char * getDestName(char * string, char * name) {
+	char *p; p=string;
+	char *n; n=name;
+	int nameLength=NICK_SIZE;
+
+	while(*p && *p != ':' && nameLength-->0)
+	{
+		if(strchr("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789_",*p)) {	// acceptable symbols for a name
+			*n++=*p++;      // move the gotten char to name char array
+		} else {
+			p++;            // if the char is not acceptable just move to another one
+		}
+	}
+	p=0; n=0;
+}
+// get a message body
+char * getMessageBody(char * string, char * message) {
+	char *p; p=string;
+	char *m; m=message;
+	int messageLength=RCVBUFSIZE;
+
+	// move the pointer until a message body
+	while(*p && *p != ':')
+	{
+		p++;
+	}
+	p++;
+
+	while(*p && *p != ':' && messageLength-->0)
+	{
+		if(strchr("'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789_!? ",*p)) {	// acceptable symbols for a name
+			*m++=*p++;		// move the gotten char to name char array
+		} else {
+			p++;          // if the char is not acceptable just move to another one
+		}
+	}
+	p=0; m=0;
+}
+/* ----------------------------------------------------------------------------------------- */
