a3d7aeaf2875f8ab8e8563edcb7bb2436c6e7fe7
zenichev <34710178+zenichev@users.noreply.github.com>
2018-09-25 11:24:45 +0300

    Update update_local_data.c

--- server/update_local_data.c
+++ server/update_local_data.c
@@ -1,13 +1,13 @@
 /* DZChat - Server. Sep 2018 @Donat Zenichev */
 /* Keep local arrays of structures updates */
 
-#include <stdio.h>        /* for printf() and fprintf() */
-#include <string.h>       /* different manipulations over a char arrays, such as memset() */
-#include <sys/socket.h>   /* for socket(), bind(), connect(), recv() and send() */
-#include <sys/types.h>    /* data types */
-#include "main.h"         /* include my custom headers and definitions */
-#include <errno.h>        /* reporting and retrieving error conditions */
-#include <stdlib.h>       /* malloc() , calloc(), free() , atof(), atoi(), atol() */
+#include <stdio.h>		/* for printf() and fprintf() */
+#include <string.h>		/* different manipulations over a char arrays, such as memset() */
+#include <sys/socket.h>		/* for socket(), bind(), connect(), recv() and send() */
+#include <sys/types.h>		/* data types */
+#include "main.h"		/* include my custom headers and definitions */
+#include <errno.h>		/* reporting and retrieving error conditions */
+#include <stdlib.h>		/* malloc() , calloc(), free() , atof(), atoi(), atol() */
 
 /* This array stores an acutal IDs of clients */
 /*----------------------------------------------------------------------------------------------------------------------------*/
@@ -82,19 +82,19 @@ int * setCurrentList(int *passedID, int action, client_parameters * infoClients)
 /* It allows a user to insert his own parameters in array of structures */
 /*----------------------------------------------------------------------------------------------------------------------------*/
 client_parameters * clientsStructure(char * name, int * myID, int * socket, char * portListening, char * ip, int usersOnline, unsigned int * counter) {
-	static client_parameters params[1000];    // structure for client parameters
+	static client_parameters params[1000];				// structure for client parameters
 
-	if (params[*myID].client_id != 0) {       // be sure that this id is not allocated yet for an other client
+	if (params[*myID].client_id != 0) {				// be sure that this id is not allocated yet for an other client
 		while(params[*myID].client_id != 0) *myID=randomID();
 	}
 
-	strcpy(params[*myID].nickname,name);      // set a client name to a structure
-	params[*myID].client_id = *myID;          // set a client id to a structure
-	params[*myID].socket = *socket;           // set a client socket to a structure
-	params[*myID].port = atoi(portListening); // set a client port to a structure
-	strcpy(params[*myID].ip,ip);              // set a client name to a structure
-	params[*myID].counter = *counter;         // set a thread number to a structure
+	strcpy(params[*myID].nickname,name);				// set a client name to a structure
+	params[*myID].client_id = *myID;				// set a client id to a structure
+	params[*myID].socket = *socket;					// set a client socket to a structure
+	params[*myID].port = atoi(portListening);			// set a client port to a structure
+	strcpy(params[*myID].ip,ip);					// set a client name to a structure
+	params[*myID].counter = *counter;				// set a thread number to a structure
 
-	return params;                            // return a shared memory with arrays of structures
+	return params;							// return a shared memory with arrays of structures
 }
 /*----------------------------------------------------------------------------------------------------------------------------*/
