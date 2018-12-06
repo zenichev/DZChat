e647a21e0ad378a0043fe153b08ea3557ce4d66c
zenichev <34710178+zenichev@users.noreply.github.com>
2018-12-06 10:26:05 +0200

    Update client_handler.c

--- server/client_handler.c
+++ server/client_handler.c
@@ -3,7 +3,7 @@
 
 #include <stdio.h>		/* for printf() and fprintf() */
 #include <string.h>		/* different manipulations over a char arrays, such as memset() */
-#include <sys/socket.h>		/* for socket(), bind(), connect(), recv() and send() */
+#include <sys/socket.h>	/* for socket(), bind(), connect(), recv() and send() */
 #include <sys/types.h>		/* data types */
 #include "main.h"		/* include my custom headers and definitions */
 
